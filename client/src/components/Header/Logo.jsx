18d3a0c0b2cf926358b9229e817a4c60c042a11f
Valentin Albornoz <valeg486@gmail.com>
2023-09-18 16:02:23 -0300

    Chaskibunes Apple Store Ecommerce
    creamos lo que viene siendo el proyecto en react, creamos las carpetas de components, layout,  header y footer

---
+++ client/src/components/Header/Logo.jsx
@@ -0,0 +1,7 @@
+import AppleLogo from '../../assets/Chaskibunes_apple_store.png';
+
+const Logo = () => {
+  return <img src={AppleLogo} alt="Apple Logo" style={{ height: '40px' }} />;
+};
+
+export default Logo;
---
+++ client/src/components/Layout/Layout.jsx
@@ -0,0 +1,10 @@
+import './layoutStyle.css';
+const layout = ({children}) => {
+  return (
+    <div>
+        <div>{children}</div> {/* Renderizamos los elementos hijos dentro de un contenedor div. */}
+    </div>
+  )
+}
+
+export default layout;
